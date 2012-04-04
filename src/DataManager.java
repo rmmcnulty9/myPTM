0dd6b3edf048fd6790ed367b1abc50b97b4a210f
Jeff <zip.github@goldsy.net>
2012-04-03 22:59:00 -0400

    Development updates.

--- src/DataManager.java
+++ src/DataManager.java
@@ -1,19 +1,21 @@
 
 public class DataManager extends Thread {
 	public Pair current_op = null;
-	
+
 	private int checkpoint_op_cnt;
 	private int num_pages;
-	
-	public DataManager(Pair _current_op, int buffer) {
+    private Scheduler parent_sched_task = null;
+
+	public DataManager(Pair _current_op, int buffer, Scheduler _parent_sched_task) {
 		current_op = _current_op;
 		num_pages = buffer;
+        parent_sched_task = _parent_sched_task;
 	}
 
 	public void run(){
-		
+
 		if(current_op!=null){
-			
+
 		}
 	}
 }
--- src/Scheduler.java
+++ src/Scheduler.java
@@ -3,25 +3,25 @@ import java.util.Map;
 
 
 public class Scheduler extends Thread{
-	
+
 	public Pair current_op = null;
-	
+
 	private ArrayList<Transaction> transactions = null;
 	private int deadlock_op_cnt;
-	
-	private DataManager dm = null;
-	
+
+	private DataManager dm_task = null;
+
 	public Scheduler(ArrayList<Transaction> _transactions, int buffer){
 		transactions = _transactions;
-		
-		if(dm == null){
-			dm = new DataManager(current_op,buffer);
+
+		if(dm_task == null){
+			dm_task = new DataManager(current_op, buffer, this);
 		}
 	}
-	
+
 	public void run(){
-		
-		
+
+
 	}
-	
+
 }
